6f6666fbe6471c884e2f9f53e7d30f59c78ab150
NorioFujii <39148371+NorioFujii@users.noreply.github.com>
2021-11-02 10:07:07 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -49,8 +49,8 @@ var Comment="", Tid=null, turnN=1, ClipDT="", W=null;
 var Rotates = new Array();
 var RotSft = 0;
 function initnotscrambled(){
-    if (window.outerWidth>600) window.resizeTo(580,440);
-    else                       window.resizeTo(380,580);
+    if (window.outerWidth>500) window.resizeTo(580,440);
+    else                       window.resizeTo(380,640);
     speed=80; if (NxPaus<1100) NxPaus=1000;
     if (turnN==16) NxPaus=1500;
     Pause=false; Comment="";RotSft=0;Rotates=[]; turnN=1; clearTimeout(Tid);
